3c3f504038b5e3fdb032c189355f9b4ba3ae4024
Matthew Moniz <Matt@Fastforward.sh>
2023-04-28 10:18:52 -0400

    Added components, sanity api info, global css

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export {default as Footer} from './Footer';
+export {default as ShoppingCart} from './ShoppingCart';
+export {default as FooterBanner} from './FooterBanner';
+export {default as HeroBanner} from './HeroBanner';
+export {default as Layout} from './Layout';
+export {default as NavBar } from './NavBar';
+export {default as Product} from './Product';
