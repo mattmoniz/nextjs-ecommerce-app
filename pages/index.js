bdbc3853210d89a36caf39bbc800c8c9005bebec
Matthew Moniz <Matt@Fastforward.sh>
2023-04-28 11:51:12 -0400

    replaced placeholders with sample product values from sanity

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,40 @@ import React from "react";
 import { client } from "@/lib/client";
 import { Product, FooterBanner, HeroBanner } from "@/components";
 
-const Home = () => {
+// products and bannerdata from bottom gets passed down as props uphere
+const Home = ({products, bannerData}) => {
+
   return (
-    <>
-      <HeroBanner />
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Many types of speakers</p>
       </div>
-      <div className= "products-container">{["product1", "product2"].map((product) => product)}</div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
       <FooterBanner />
-    </>
+    </div>
   );
 };
+
+export const getServerSideProps = async () => {
+  // grab all of products from sanity dashboard
+  const productQuery = '*[_type == "product"]'
+  const products = await client.fetch(productQuery);
+
+  // grab all of products from sanity dashboard
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+
+
+}
+
 
 export default Home;
