07dc8c95958a5e7e53b571e4e0e995ff498f9c19
Matthew Moniz <Matt@Fastforward.sh>
2023-05-03 12:00:55 -0400

    updated the footerbanner,Layout, Navbar, and product components with new content format and wrapped Component in _app.js in the layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
-const Layout = () => {
+// this file will be called from _app.js
+
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Matt's Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,18 @@ const Home = ({products, bannerData}) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-    {console.log(bannerData)}
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Many types of speakers</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
