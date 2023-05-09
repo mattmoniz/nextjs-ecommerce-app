0e8295cd384f0f5c5152c4497786d4ca1a855374
Matthew Moniz <Matt@Fastforward.sh>
2023-05-09 14:55:23 -0400

    updated navbar with shopping cart component

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 import {AiOutlineShopping} from 'react-icons/ai'
+import {ShoppingCart} from './';
+import { useStateContext } from '@/context/StateContext';
 
 const NavBar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Matt's Techstore </Link>
       </p>
-      <button
-      type="button"
-      className="cart-icon">
+      <button type="button" className="cart-icon" onClick={()=>setShowCart(true)}>
         <AiOutlineShopping />
-        <span className = "cart-item-qty"> 1 </span>
+        <span className = "cart-item-qty"> {totalQuantities} </span>
       </button>
+
+      {showCart && <ShoppingCart />}
+
     </div>
   )
 }
--- context/StateContext.js
+++ context/StateContext.js
@@ -5,8 +5,8 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
   const increaseQty = () => {
