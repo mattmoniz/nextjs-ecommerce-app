6c162e7fb1b96f21a378db8a02aba8abde01e2ad
Matthew Moniz <Matt@Fastforward.sh>
2023-05-16 15:21:45 -0400

    rounded subtotal in cart

--- components/ShoppingCart.jsx
+++ components/ShoppingCart.jsx
@@ -93,7 +93,7 @@ const ShoppingCart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>{totalPrice}</h3>
+              <h3>{totalPrice.toFixed(2)}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick="">
